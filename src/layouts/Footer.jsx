04c36315bc55f1b64b213e8b8570fe399d2a0aa6
juan <jpredcoin@gmail.com>
2023-07-26 21:44:42 -0500

    lista para enviar  pero falta por completar

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,29 +1,19 @@
-// src/layouts/Footer.js
-
 import React from 'react';
-import './Footer.css'; // Agrega el archivo de estilos CSS correspondiente
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>SPA - TCC diseñada por Juan Pablo Rios Betancur &copy; {new Date().getFullYear()}</p>
-      <ul>
-        <li>
-          <a href="https://github.com/tu_usuario_github" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/tu_canal_youtube" target="_blank" rel="noopener noreferrer">
-            YouTube
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/tu_perfil_instagram" target="_blank" rel="noopener noreferrer">
-            Instagram
-          </a>
-        </li>
-      </ul>
+      <div className="footer-content">
+        <p>SPA - TCC diseñada por Juan Pablo Rios Betancur © {new Date().getFullYear()}</p>
+        <div className="footer-links">
+          <a href="https://github.com/tu_usuario_github" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <span className="footer-divider">|</span>
+          <a href="https://www.youtube.com/tu_canal" target="_blank" rel="noopener noreferrer">YouTube</a>
+          <span className="footer-divider">|</span>
+          <a href="https://www.instagram.com/tu_perfil_instagram" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+      </div>
     </footer>
   );
 };
