b6f88512ec20b011bcc96d62173fdfa46586b16c
juan <jpredcoin@gmail.com>
2023-07-26 21:03:08 -0500

    Finalizando tiempo de entrega 2 App

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -6,8 +6,24 @@ import './Footer.css'; // Agrega el archivo de estilos CSS correspondiente
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Derechos de autor &copy; {new Date().getFullYear()} TCC</p>
-      {/* Agrega enlaces a páginas importantes o iconos de redes sociales si es relevante */}
+      <p>SPA - TCC diseñada por Juan Pablo Rios Betancur &copy; {new Date().getFullYear()}</p>
+      <ul>
+        <li>
+          <a href="https://github.com/tu_usuario_github" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/tu_canal_youtube" target="_blank" rel="noopener noreferrer">
+            YouTube
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/tu_perfil_instagram" target="_blank" rel="noopener noreferrer">
+            Instagram
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
